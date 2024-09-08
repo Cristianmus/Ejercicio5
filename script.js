b3912d08aa734d050f309a1e244abb103ebb90bc
Néstor <168029433+NestorP255@users.noreply.github.com>
2024-09-08 13:11:49 -0700

    Implementación de JS

---
+++ script.js
@@ -0,0 +1,12 @@
+function getColorAleatorio() {
+    const letras = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letras[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function cambiarColorFondo() {
+    document.body.style.backgroundColor = getColorAleatorio();
+}
